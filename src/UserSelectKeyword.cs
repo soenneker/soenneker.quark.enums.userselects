8704bab23b082a1093f813f113f04fe4580ef4a0
Jake Soenneker <jake@soenneker.com>
2026-02-27 08:24:45 -0600

    Deps

--- src/UserSelectKeyword.cs
+++ src/UserSelectKeyword.cs
@@ -6,11 +6,10 @@ namespace Soenneker.Quark.Enums;
 /// Typed values for the CSS user-select property in .NET enumeration form.
 /// </summary>
 [EnumValue<string>]
+[IncludeEnumValues(typeof(GlobalKeyword))]
 public sealed partial class UserSelectKeyword
 {
     public static readonly UserSelectKeyword None = new("none");
     public static readonly UserSelectKeyword Auto = new("auto");
     public static readonly UserSelectKeyword All = new("all");
-
-    public static implicit operator UserSelectKeyword(GlobalKeyword style) => new(style.Value);
 }
